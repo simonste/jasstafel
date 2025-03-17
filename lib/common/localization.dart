5fd84b8a4c53cbe1b98e258c0cd707a0df7df240
Simon Steinmann <simonste3@gmail.com>
2025-03-17 22:16:30 +0100

    https://docs.flutter.dev/release/breaking-changes/flutter-generate-i10n-source

--- lib/common/localization.dart
+++ lib/common/localization.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:jasstafel/l10n/app_localizations.dart';
 
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+export 'package:jasstafel/l10n/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this)!;
