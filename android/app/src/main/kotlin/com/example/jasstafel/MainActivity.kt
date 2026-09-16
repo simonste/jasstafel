3ea14c7b7134d3d83aef0f37705f246df1ac0d5e
Simon Steinmann <simonste3@gmail.com>
2026-09-16 20:13:55 +0200

    fix EdgeToEdge on Android SDK 35

--- android/app/src/main/kotlin/com/example/jasstafel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/jasstafel/MainActivity.kt
@@ -1,6 +1,18 @@
 package ch.simonste.jasstafel
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Enable edge-to-edge for Android 10+
+        // WindowCompat.setDecorFitsSystemWindows replaces the deprecated APIs
+        // setStatusBarColor/setNavigationBarColor which are deprecated in Android 15
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+        super.onCreate(savedInstanceState)
+    }
 }
